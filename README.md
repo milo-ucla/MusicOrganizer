dcebd32f6d324c71c94df48872a50fefb5e125ed
milo-ucla <milok23@ucla.edu>
2022-02-07 02:00:55 -0800

    add_music.py: I cha nged the main python function to correctly go into the API keys and authenticate for a specific user (me).  THere is another way to authenticate for general API calls that is faster, but it can only do non-user-sp ecific calls like search queries. The code now searches for the top result based on the text file songs.txt (mandatory file). It will then add all of those songs to the queue for the active user. This function is async, TBD if that actually fixes the issue where API calls sometimes are null when they are sent to Sp otify... > ## Local Environment Variable added (.env) and now included in gitignore ## Updated requirements

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MusicOrganizer
 First, run the command
 
-`$python3 -m venv ./env` to create a virtual environment in which to store your dependencies. 
+`$python3 -m venv ./.venv` to create a virtual environment in which to store your dependencies. 
 
-Next, run `$source env/bin/activate` in the project directory to start your venv. Once you are in a venv, you can install all dependencies for this project by running `$python3 -m pip install -r requirements.txt`
+Next, run `$source .venv/bin/activate` in the project directory to start your venv. Once you are in a venv, you can install all dependencies for this project by running `$python3 -m pip install -r requirements.txt`
--- add_music.py
+++ add_music.py
@@ -1,18 +1,39 @@
 #!/bin/python3
-
-#this is just an example
-
+import os
+from pprint import pprint
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+#from spotipy.oauth2 import SpotifyClientCredentials #this is only for non-user API calls
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+import asyncio
 
-birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+load_dotenv()
 
-results = spotify.artist_albums(birdy_uri, album_type='album')
-albums = results['items']
-while results['next']:
-    results = spotify.next(results)
-    albums.extend(results['items'])
+SPOTIPY_CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
+SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
+SPOTIPY_REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI')
 
-for album in albums:
-    print(album['name'])
+#Documentation: https://developer.spotify.com/documentation/general/guides/authorization/scopes/
+scope='streaming'
+
+spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+async def main():
+    result = []
+    track_uris = []
+
+    with open('songs.txt') as f:
+        tracks_to_add = f.readlines()
+
+    for title in tracks_to_add:
+        if not(title == "" or ('#' in title)):
+            result = spotify.search(q='track:' + title, type='track',limit=1)
+            track_uris.append(result['tracks']['items'][-1]['uri'])
+
+    for uri in track_uris:
+        spotify.add_to_queue(uri=uri, device_id=None)
+    
+    return 0
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
